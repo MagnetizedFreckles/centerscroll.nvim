a7ee91a69566ecfbf5dc022fb0c271447319760e
MagnetizedFreckles <magnetizedfreckles@disroot.org>
2024-01-03 00:53:55 +0300

    i hope this works

---
+++ lua/plugin.lua
@@ -0,0 +1,34 @@
+function CenterCursor()
+    vim.api.nvim_feedkeys("zz", "nt", true)
+end
+
+function CenterScroll()
+    local visible_lines = vim.fn.winheight(0)
+    local screen_center = math.ceil(visible_lines / 2)
+    local scrolloff_value = screen_center - 1
+
+    local distance_to_eof = vim.fn.line("$") - vim.fn.line(".")
+
+    if distance_to_eof <= screen_center then
+        if vim.opt.scrolloff:get() ~= 0 then
+            vim.opt.scrolloff = 0
+            CenterCursor()
+        else
+            CenterCursor()
+        end
+    elseif distance_to_eof > screen_center then
+        if vim.opt.scrolloff:get() ~= scrolloff_value then
+            vim.opt.scrolloff = scrolloff_value
+        end
+    end
+end
+
+local checkline = nil
+vim.api.nvim_create_autocmd("CursorMoved", {
+    callback = function()
+        if vim.fn.line(".") ~= checkline then
+            CenterScroll()
+            checkline = vim.fn.line(".")
+        end
+    end,
+})
